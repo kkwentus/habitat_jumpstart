b9b8a9a4f73798c723713d9213928344fc2eb5e9
Habitat User <hab@ip-10-0-1-72.us-west-2.compute.internal>
2018-06-12 01:43:04 +0000

    my workshop updates

--- national-parks/habitat/plan.sh
+++ national-parks/habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
-pkg_origin=kkw-sa-wkrsp
+pkg_origin=
 pkg_version=1.0.0
 pkg_maintainer="Bill Meyer <b@chef.io>"
 pkg_license=('Apache-2.0')
